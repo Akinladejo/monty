92c419fbaf38b9efd29a9a82d32bf4a017f61294
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-02-04 01:31:54 +0100

    Update push.c

--- push.c
+++ push.c
@@ -44,3 +44,26 @@ void op_push(stack_t **sp)
 		(*sp) = new;
 	}
 }
+
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * op_pall - print each element on the stack
+ * @sp: stack pointer
+ */
+
+void op_pall(stack_t **sp)
+{
+	stack_t *p = NULL;
+
+	if (*sp)
+	{
+		(*sp)->next->prev = NULL;
+
+		for (p = *sp; p; p = p->prev)
+			printf("%d\n", p->n);
+
+		(*sp)->next->prev = *sp;
+	}
+}
